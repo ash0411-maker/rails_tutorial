381d70ad7e5b632147265dfe282631b5e02bd654
Uetani <satoshi.0411@icloud.com>
2021-01-04 11:12:39 +0900

    [add] about action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
+    # 純粋なrubyであればここに何も記述していないと何も実行しない。
+    # しかし、ApplicationControllerを継承しているからrails特有の振る舞いをする。
   end
 
   def help
+  end
+
+  def about
   end
 end
