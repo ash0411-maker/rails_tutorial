7d710f3e107029461b609e11e820867a59ae8f7e
ash <satoshi.0411@icloud.com>
2021-01-09 11:01:04 +0900

    [add] cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,15 @@
 module SessionsHelper
+
   def log_in(user)
     # sessionメソッドで作成された一時cookiesは、ブラウザを閉じた瞬間に有効期限が終了
     session[:user_id] = user.id #sessionコントローラーとは無関係
+  end
+
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   def current_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   validates :password, presence: true, length: { minimum: 4 }
@@ -10,4 +12,26 @@ class User < ApplicationRecord
   # 2つのペアの仮想的な属性 (passwordとpassword_confirmation) が使えるようになる。
   # authenticateメソッドをオブジェクトに対して使えるようになる。
   has_secure_password
+
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # cookiesの保存場所。データベースには保存しない。
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
